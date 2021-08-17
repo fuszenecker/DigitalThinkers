ef6e0748949db8acacdb175c500559847699939b
Róbert Fuszenecker <robert.fuszenecker@betssongroup.com>
2021-08-17 11:48:56 +0200

    Added "/api" prefix to public API endpoints.

--- src/api/Controllers/WeatherForecastController.cs
+++ src/api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace DigitalThinkers.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
