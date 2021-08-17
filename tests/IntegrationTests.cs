8fa4fbb1bc1cf2cf0146d542aa70aa6c507b6750
Róbert Fuszenecker <robert.fuszenecker@betssongroup.com>
2021-08-17 17:41:05 +0200

    Added few integration tests.

--- tests/IntegrationTests.cs
+++ tests/IntegrationTests.cs
@@ -1,7 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,31 +15,35 @@ namespace tests
         const string serviceBaseUrl = "http://api:5000";
         const string HealthCheckEndpointPath = "/health";
         const string MetricsEndpointPath = "/metrics";
+        const string StockEndpointPath = "/api/v1/Stock";
+        const string CheckoutEndpointPath = "/api/v1/Checkout";
 
         const string CorrelationIdHeaderName = "X-Correlation-ID";
 
-        HttpClient httpClient = new HttpClient();
+        private HttpClient httpClient;
 
-        public IntegrationTests()
+        [TestInitialize]
+        public void Initialize()
         {
+            httpClient = new HttpClient() {
+                BaseAddress = new Uri(serviceBaseUrl)
+            };
         }
 
         [TestMethod]
-        [Ignore]
         public async Task HealthCheckWorks()
         {
-            var result = await httpClient.GetAsync(serviceBaseUrl + HealthCheckEndpointPath);
+            var result = await httpClient.GetAsync(HealthCheckEndpointPath).ConfigureAwait(false);
 
             Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
-            Assert.AreEqual("Healthy", await result.Content.ReadAsStringAsync());
+            Assert.AreEqual("Healthy", await result.Content.ReadAsStringAsync().ConfigureAwait(false));
         }
 
         [TestMethod]
-        [Ignore]
         public async Task MetricsWorks()
         {
-            var result = await httpClient.GetAsync(serviceBaseUrl + MetricsEndpointPath);
-            var responseBody = await result.Content.ReadAsStringAsync();
+            var result = await httpClient.GetAsync(MetricsEndpointPath).ConfigureAwait(false);
+            var responseBody = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
 
             Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
 
@@ -46,17 +52,81 @@ namespace tests
         }
 
         [TestMethod]
-        [Ignore]
         public async Task CorrelationIdWorks()
         {
             var correlationId = Guid.NewGuid().ToString();
 
             httpClient.DefaultRequestHeaders.Add(CorrelationIdHeaderName, correlationId);
 
-            var result = await httpClient.GetAsync(serviceBaseUrl + MetricsEndpointPath);
+            var result = await httpClient.GetAsync(MetricsEndpointPath).ConfigureAwait(false);
 
             Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
             Assert.AreEqual(correlationId, result.Headers.GetValues("X-Correlation-ID").First());
+        }
+
+        [TestMethod]
+        public async Task GetStock()
+        {
+            var result = await httpClient.GetAsync(StockEndpointPath).ConfigureAwait(false);
+
+            var stock = JsonSerializer.Deserialize<Dictionary<uint, uint>>(await result.Content.ReadAsStringAsync());
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
+            Assert.AreEqual(0, stock.Count);
+        }
+
+        [TestMethod]
+        public async Task PostStock()
+        {
+            var json = "{ \"20\": 5, \"1\": 100}";
+            var data = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            var result = await httpClient.PostAsync(StockEndpointPath, data).ConfigureAwait(false);
+
+            var stock = JsonSerializer.Deserialize<Dictionary<uint, uint>>(
+                await result.Content.ReadAsStringAsync().ConfigureAwait(false));
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
+            Assert.IsTrue(stock.Count > 0);
+        }
+
+
+        [TestMethod]
+        public async Task PostBadStock()
+        {
+            var json = "{ \"xxx\": 5, \"1\": 100}";
+            var data = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            var result = await httpClient.PostAsync(StockEndpointPath, data).ConfigureAwait(false);
+
+            Assert.AreEqual(System.Net.HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task CheckoutStock()
+        {
+            var json = "{ \"20\": 5, \"1\": 100}";
+            var data = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            var result = await httpClient.PostAsync(StockEndpointPath, data).ConfigureAwait(false);
+
+            var stock = JsonSerializer.Deserialize<Dictionary<uint, uint>>(
+                await result.Content.ReadAsStringAsync().ConfigureAwait(false));
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
+            Assert.IsTrue(stock.Count > 0);
+
+            var checkoutJson = "{ \"inserted\": {\"20\": 3, \"1\": 100}, \"price\": 50 }";
+            var checkoutData = new StringContent(checkoutJson, System.Text.Encoding.UTF8, "application/json");
+
+            var checkoutResult = await httpClient.PostAsync(CheckoutEndpointPath, checkoutData).ConfigureAwait(false);
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, checkoutResult.StatusCode);
+
+            var payBack = JsonSerializer.Deserialize<Dictionary<uint, uint>>(
+                await checkoutResult.Content.ReadAsStringAsync().ConfigureAwait(false));
+
+            Assert.IsTrue(payBack.Count > 0);
         }
     }
 }
