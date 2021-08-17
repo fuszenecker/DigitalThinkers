00ddb47865ce4c465638b0f5bbbb02501a0f8534
Róbert Fuszenecker <robert.fuszenecker@betssongroup.com>
2021-08-17 10:55:34 +0200

    Added integration tests.

--- tests/IntegrationTests.cs
+++ tests/IntegrationTests.cs
@@ -1,13 +1,41 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace tests
 {
     [TestClass]
     public class IntegrationTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        const string serviceBaseUrl = "http://api:5000";
+        const string HealthCheckEndpointPath = "/health";
+        const string MetricsEndpointPath = "/metrics";
+
+        HttpClient httpClient = new HttpClient();
+
+        public IntegrationTests()
         {
+        }
+
+        [TestMethod]
+        public async Task HealthCheckWorks()
+        {
+            var result = await httpClient.GetAsync(serviceBaseUrl + HealthCheckEndpointPath);
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
+            Assert.AreEqual("Healthy", await result.Content.ReadAsStringAsync());
+        }
+
+        [TestMethod]
+        public async Task MetricsWorks()
+        {
+            var result = await httpClient.GetAsync(serviceBaseUrl + MetricsEndpointPath);
+            var responseBody = await result.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
+
+            // Response body contains at least 20 lines:
+            Assert.IsTrue(responseBody.Split('\n').Length > 20);
         }
     }
 }
--- tests/UnitTests.cs
+++ tests/UnitTests.cs
@@ -5,9 +5,9 @@ namespace tests
     [TestClass]
     public class UnitTests
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        // [TestMethod]
+        // public void TestMethod1()
+        // {
+        // }
     }
 }
