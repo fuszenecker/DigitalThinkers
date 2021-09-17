7116f8656484a48a37d92f3e2f4c151a7c84ae5c
Fuszenecker Róbert <robert.fuszenecker@outlook.com>
2021-09-17 15:12:13 +0200

    Added Loki logging.

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -76,7 +76,7 @@ namespace DigitalThinkers
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory, IApiVersionDescriptionProvider provider)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || true)
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
